177aa6505d81464628051595816d46bafe50fe83
LoneWolf <imalkesara2001@gmail.com>
2024-05-19 19:06:32 +0530

    fixed help of chatgpt :(

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
-<script>
+<!-- <script>
 // @ts-nocheck
 
-  import {onMount} from 'svelte';
+  import { onMount } from 'svelte';
 	import { musicList } from './musiclist.js';
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
@@ -18,15 +18,16 @@
       currentSongIndex = (currentSongIndex - 1) % musicList.length;
     }
     playerState = "play";
+    audioElement.play();
 
   }
 
   function playPause(){
-    if(playPause == "play"){
+    if(playerState == "play"){
       playerState == "pause";
       audioElement.pause();
     }else{
-      playerState == "play";
+      playerState = "play";
       audioElement.play();
     }
 
@@ -35,11 +36,17 @@
   function next(){
     currentSongIndex = (currentSongIndex + 1) % musicList.length;
     playerState = "play";
+    audioElement.play();
   }
 
-  function setSong(){
-
+  function setSong(i){
+    currentSongIndex = i;
+    playerState = "play";
   }
+
+  onMount(() => {
+    audioElement.play();
+  });
 
 
 </script>
@@ -49,7 +56,7 @@
   <audio
     src={'./files/audio/' + $musicList[currentSongIndex].audio}
     bind:this = {audioElement}
-    autoplay = "false"
+    autoplay = false
   >
 
   </audio>
@@ -105,3 +112,126 @@
 <style>
   
 </style>
+ -->
+   <script>
+    // @ts-nocheck
+    
+    import { onMount } from 'svelte';
+    import { musicList } from './musiclist.js';
+    import svelteLogo from './assets/svelte.svg';
+    import viteLogo from '/vite.svg';
+    
+    let currentSongIndex = 0;
+    let playerState = "play";
+    let audioElement;
+    let mainElement;
+    
+    function updateAudioSource() {
+      if ($musicList && musicList[currentSongIndex]) {
+        audioElement.src = './files/audio/' + $musicList[currentSongIndex].audio;
+      }
+    }
+    
+    function prev() {
+      if (currentSongIndex === 0) {
+        currentSongIndex = $musicList.length - 1;
+      } else {
+        currentSongIndex = (currentSongIndex - 1) % $musicList.length;
+      }
+      playerState = "play";
+      updateAudioSource();
+    }
+    
+    function playPause() {
+      if (playerState === "play") {
+        playerState = "pause";
+        audioElement.pause();
+      } else {
+        playerState = "play";
+        audioElement.play();
+      }
+    }
+    
+    function next() {
+      currentSongIndex = (currentSongIndex + 1) % $musicList.length;
+      playerState = "play";
+      updateAudioSource();
+    }
+    
+    function setSong(i) {
+      currentSongIndex = i;
+      playerState = "play";
+      updateAudioSource();
+    }
+    
+    onMount(() => {
+      if ($musicList && $musicList.length > 0) {
+        updateAudioSource();
+        audioElement.addEventListener('canplaythrough', () => {
+          if (playerState === "play") {
+            audioElement.play();
+          }
+        });
+      }
+    });
+    </script>
+    
+    <main bind:this={mainElement}>
+    
+      
+      <audio
+        src={'./files/audio/' + $musicList[currentSongIndex].audio}
+        bind:this = {audioElement}
+        autoplay = false
+      >
+
+      </audio>
+    
+      <div class="player">
+        <div class="current-song">
+          <div class="avatar">
+            <img src={'./files/images/' + ($musicList[currentSongIndex]?.image || '')} alt="">
+          </div>
+          <div class="song-controls">
+            <h2>{$musicList[currentSongIndex]?.name || 'No song selected'}</h2>
+            <div class="controls">
+              <button on:click={prev}>
+                <i class="fa fa-backward"></i>
+              </button>
+    
+              <button on:click={playPause}>
+                {#if playerState === "play"}
+                  <i class="fa fa-pause"></i>
+                {:else}
+                  <i class="fa fa-play"></i>
+                {/if}
+              </button>
+    
+              <button on:click={next}>
+                <i class="fa fa-forward"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+    
+        <div class="song-list">
+          {#each $musicList as music, i}
+            <div class="{i === currentSongIndex ? 'active' : ''}" on:click={() => setSong(i)}>
+              <div class="avatar">
+                <img src={'./files/images/' + music.image} alt="">
+              </div>
+              <div class="song-details">
+                <h2>{music.name}</h2>
+                <p>{music.artist}</p>
+              </div>
+            </div>
+          {/each}
+        </div>
+      </div>
+      
+    </main>
+    
+    <style>
+      /* Add your styles here */
+    </style>
+    
--- src/musiclist.js
+++ src/musiclist.js
@@ -2,17 +2,15 @@ import { writable } from 'svelte/store';
 
 export const musicList = writable([
   {
+    image: 'evergreen.jpg',
+    audio: 'evergrren.mp3',
+    name: 'EverGreen',
+    artist: 'Mitch',
+  },
+  {
     image: 'maxresdefault.jpg',
     audio: 'Djo_-_End_Of_Beginning_(Hydr0.org).mp3',
     name: 'End of Begining',
     artist: 'Djo',
-  },
-
-  {
-    image: 'evergreen.jpg',
-    audio:
-      'Richy_Mitch_and_The_Coal_Miners_-_Evergreen_instrumental_(Hydr0.org).mp3',
-    name: 'EverGreen',
-    artist: 'Mitch',
   },
 ]);
