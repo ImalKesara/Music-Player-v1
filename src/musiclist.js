84239799316363bd90c27125a19de297f8e5bb21
LoneWolf <imalkesara2001@gmail.com>
2024-05-19 17:26:40 +0530

    added image and audio to a json object

---
+++ src/musiclist.js
@@ -0,0 +1,10 @@
+import { writable } from 'svelte/store';
+
+export const musicList = writable([
+  {
+    image: 'maxresdefault.jpg',
+    audio: 'Djo_-_End_Of_Beginning_(Hydr0.org).mp3',
+    name: 'End of Begining',
+    artist: 'Djo',
+  },
+]);
