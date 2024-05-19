4183f84a1d9ff29876d32413884a00e3d0566995
LoneWolf <imalkesara2001@gmail.com>
2024-05-19 19:47:20 +0530

    mostly done

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <!-- <script>
+	import Counter from './lib/Counter.svelte';
 // @ts-nocheck
 
   import { onMount } from 'svelte';
@@ -139,6 +140,7 @@
         currentSongIndex = (currentSongIndex - 1) % $musicList.length;
       }
       playerState = "play";
+      setBackground()
       updateAudioSource();
     }
     
@@ -155,16 +157,19 @@
     function next() {
       currentSongIndex = (currentSongIndex + 1) % $musicList.length;
       playerState = "play";
+      setBackground()
       updateAudioSource();
     }
     
     function setSong(i) {
       currentSongIndex = i;
       playerState = "play";
+      setBackground()
       updateAudioSource();
     }
     
     onMount(() => {
+      setBackground();
       if ($musicList && $musicList.length > 0) {
         updateAudioSource();
         audioElement.addEventListener('canplaythrough', () => {
@@ -174,6 +179,15 @@
         });
       }
     });
+
+    function setBackground(){
+      let background = `
+        linear-gradient(rgba(0,0,0,0.45),rgba(0,0,0,0.5)),
+        url(./files/images/${$musicList[currentSongIndex].image}) center no-repeat
+      `;
+        mainElement.style.background = background;
+        mainElement.style.backgroundSize = "cover";
+    }
     </script>
     
     <main bind:this={mainElement}>
@@ -232,6 +246,144 @@
     </main>
     
     <style>
-      /* Add your styles here */
+      main{
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+      }
+      audio{
+        display: none;
+      }
+      .player{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        width: 500px;
+        height: 500px;
+        display: flex;
+        flex-direction: column;
+        border-radius: 20px;
+        overflow: hidden;
+      }
+      .player .current-song{
+        height: 120px;
+        padding: 10px;
+        display: flex;
+        background: rgba(255, 255, 255, 0.8);
+      }
+      .player .current-song .avatar{
+        width: 100px;
+        height: 100px;
+        padding: 10px;
+        text-align: center;
+      }
+      .player .current-song .avatar img{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        object-fit: cover;
+      }
+
+      .player .current-song .song-controls{
+        padding-left: 10px;
+        flex: 1;
+
+      }
+
+      .player .current-song  .song-controls h2{
+        margin-bottom: 15px;
+        font-size: 20px;
+        color: #111;
+      }
+
+      .player .current-song  .song-controls .controls{
+        display: flex;
+        justify-content: space-between;
+        padding-right: 40px;
+
+      }
+
+      .player .current-song  .song-controls .controls button{
+       outline: none;
+       border: none;
+       background: transparent;
+       color: #111;
+       font-size: 20px;
+       cursor: pointer;
+        
+      }
+
+      .player .song-list{
+        height: calc(100% - 120px);
+        background: rgba(255, 255, 255,0.2);
+        box-shadow: 0px 8px 32px 0 rgba(32, 38, 135,0.2);
+        backdrop-filter: blur(5px);
+        border: 1px solid rgba(255, 255, 255, 0.4);
+        overflow-y: auto;
+      }
+
+      .player .song-list::-webkit-scrollbar{
+        width: 4px;
+        background: transparent;
+      }
+
+      .player .song-list::-webkit-scrollbar-thumb{
+        width: 4px;
+        background: #fff;
+      }
+
+      .player .song-list > div{
+        display: flex;
+        border-bottom:1px solid rgba(255, 255, 255, 0.25);
+        cursor: pointer;
+      }
+
+      .player .song-list > div.active{
+        background:  rgba(255, 255, 255, 0.25);
+      }
+
+      .player .song-list > div .avatar{
+        width: 50px;
+        height: 50px;
+        text-align: center;
+        padding: 10px;
+      }
+      .player .song-list > div .avatar img{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+          object-fit: cover;
+      }
+
+      .player .song-list > div .song-details{
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+
+      .player .song-list > div .song-details h2{
+        font-size: 16px;
+        margin: 0px 0px 2px;
+        color: #fff; 
+      }
+
+
+      .player .song-list > div .song-details p{
+        font-size: 15px;
+        margin: 0px;
+        color: #eee; 
+      }
+
+
+
+
+
+
+
+
     </style>
     
