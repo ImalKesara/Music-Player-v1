5cec8ab29f7cd71a54a62890fb6f8d42284da921
LoneWolf <imalkesara2001@gmail.com>
2024-05-19 17:38:23 +0530

    added image and audio

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,43 @@
 <script>
+// @ts-nocheck
+
+	import { musicList } from './musiclist.js';
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
+  import {onMount} from 'svelte';
+
+  let currentSongIndex = 0;
+  let playerState = "play";
+  let audioElement;
+  let mainElement;
+
+
 </script>
 
-<main>
+<main bind:this = {mainElement}>
 
-  <h1>hello wrold</h1>
+  <audio
+    src={'./files/audio/' + $musicList[currentSongIndex].audio}
+    bind:this = {audioElement}
+    autoplay = "false"
+  >
+
+  </audio>
+
+  <div class="player">
+    <div class="current-song">
+      <div class="avatar">
+        <img src={'./files/images/' +$musicList[currentSongIndex].image} alt="">
+      </div>
+      <div class="song-controls">
+          <h2>{$musicList[currentSongIndex].name}</h2>
+          <div class="controls">
+            <button on:click={prev}>
+            </button>
+          </div>
+      </div>
+    </div>
+  </div>
   
 </main>
 
